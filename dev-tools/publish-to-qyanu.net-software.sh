3b0cd6ca92066f99b75998bfab155601418c6f0a
Max-Julian Pogner <max-julian@pogner.at>
2021-09-11 21:35:28 +0200

    adjust publish script to changes in homepage

--- dev-tools/publish-to-qyanu.net-software.sh
+++ dev-tools/publish-to-qyanu.net-software.sh
@@ -6,7 +6,7 @@ MYDIR="$(realpath "$(dirname "$0")")"
 #
 # upload the latest package to qyanu.net/software
 #
-
+PACKAGE=acme-wrapper
 
 
 # need variable "OPERATIONS_BASEDIR"
@@ -17,7 +17,7 @@ MYDIR="$(realpath "$(dirname "$0")")"
 
 # check validity of existing checksum
 (
-    cd "$OPERATIONS_BASEDIR/source/_packages"
+    cd "$OPERATIONS_BASEDIR/source/_packages/${PACKAGE}"
     gpg --decrypt SHA256SUM.signed \
         | sha256sum --check
 )
@@ -32,24 +32,25 @@ VERSION="$(<./VERSION)"
 make package
 
 install -o "$USER" -g "$GROUP" --mode=a=rX,u+w \
-    -t "$OPERATIONS_BASEDIR/source/_packages/" \
-    "acme-wrapper_$VERSION.tar.bz2"
+    -t "$OPERATIONS_BASEDIR/source/_packages/${PACKAGE}/" \
+    "${PACKAGE}_$VERSION.tar.bz2"
 
 (
-    cd "$OPERATIONS_BASEDIR/source/_packages"
-    sha256sum --binary acme-wrapper_* > SHA256SUM
+    cd "$OPERATIONS_BASEDIR/source/_packages/${PACKAGE}/"
+    rm -f SHA256SUM.signed SHA256SUM
+    sha256sum --binary ${PACKAGE}_* > SHA256SUM
     gpg --clearsign --output SHA256SUM.signed SHA256SUM
     chown "${USER}:${GROUP}" SHA256SUM.signed SHA256SUM
     chmod a=rX,u+w SHA256SUM.signed SHA256SUM
 )
 (
     cd "$OPERATIONS_BASEDIR"
-    sed -re "s/acme-wrapper_([0-9]+\.){3}/acme-wrapper_${VERSION}./" \
-        -i "source/acme-wrapper/index.rst"
+    sed -re "s/${PACKAGE}_([0-9]+\.){3}/${PACKAGE}_${VERSION}./g" \
+        -i "source/${PACKAGE}/index.rst"
     git diff HEAD
     read -p "accept and commit changes? (Ctrl+C to abort, any key to continue)"
     git add -A
-    git commit -m "publish acme-wrapper v${VERSION}"
+    git commit -m "publish ${PACKAGE} v${VERSION}"
     make clean html
     git add -A
     git commit -m 'exec `make clean html`'
